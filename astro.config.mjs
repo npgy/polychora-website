813ee962f8c4aeae36d8a5a9e51a8f34ad1451e8
npgy <me@np.gy>
2025-02-23 14:00:19 -0500

    add tailwind and separate components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import compress from "astro-compress";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,7 @@ export default defineConfig({
     inlineStylesheets: "always",
   },
   integrations: [compress(), sitemap()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
